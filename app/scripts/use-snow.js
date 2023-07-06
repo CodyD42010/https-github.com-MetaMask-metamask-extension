4ad89f123720aff90ea8fb71e88c5c602f5f5b2b
Dan J Miller <danjm.com@gmail.com>
2023-07-06 14:32:23 -0230

    Revert "fix: skip snow usage for MV3 test build (#19827)"
    This reverts commit a3f811957eb20224c22e6891db2427cff7c986d9.

--- app/scripts/use-snow.js
+++ app/scripts/use-snow.js
@@ -8,22 +8,14 @@ Changing this code must be done cautiously to avoid breaking the app!
 // eslint-disable-next-line import/unambiguous
 (function () {
   const log = console.log.bind(console);
-  // eslint-disable-next-line no-undef
-  const isWorker = !self.document;
   const msg =
     'Snow detected a new realm creation attempt in MetaMask. Performing scuttling on new realm.';
-  // eslint-disable-next-line no-undef
-  Object.defineProperty(self, 'SCUTTLER', {
+  Object.defineProperty(window.top, 'SCUTTLER', {
     value: (realm, scuttle) => {
-      if (isWorker) {
-        scuttle(realm);
-      } else {
-        // eslint-disable-next-line no-undef
-        self.SNOW((win) => {
-          log(msg, win);
-          scuttle(win);
-        }, realm);
-      }
+      window.top.SNOW((win) => {
+        log(msg, win);
+        scuttle(win);
+      }, realm);
     },
   });
 })();
