d370e172a2c357acf45a547e34dd5700765cf1c7
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-03-13 20:25:01 +0900

    Disable services

--- app/scripts/controllers/preferences.js
+++ app/scripts/controllers/preferences.js
@@ -215,6 +215,9 @@ export default class PreferencesController {
 
   setDisableExternalServices(disableExternalServices) {
     this.store.updateState({ disableExternalServices });
+    this.setUseTokenDetection(!disableExternalServices);
+    this.setUseCurrencyRateCheck(!disableExternalServices);
+    this.setUsePhishDetect(!disableExternalServices);
   }
 
   /**
