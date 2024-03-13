e711a1553861230109e4b1f96b04a6121c545cbc
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-03-13 22:53:36 +0900

    Add copy, fix toggle state

--- app/scripts/controllers/preferences.js
+++ app/scripts/controllers/preferences.js
@@ -215,9 +215,9 @@ export default class PreferencesController {
 
   setDisableExternalServices(disableExternalServices) {
     this.store.updateState({ disableExternalServices });
-    this.setUseTokenDetection(!disableExternalServices);
-    this.setUseCurrencyRateCheck(!disableExternalServices);
-    this.setUsePhishDetect(!disableExternalServices);
+    this.setUseTokenDetection(disableExternalServices);
+    this.setUseCurrencyRateCheck(disableExternalServices);
+    this.setUsePhishDetect(disableExternalServices);
   }
 
   /**
