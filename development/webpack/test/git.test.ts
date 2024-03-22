817ada73702cbd2b0cbed9f7de54eecc0cb1f9a2
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2024-03-21 20:05:07 -0400

    more git

--- development/webpack/test/git.test.ts
+++ development/webpack/test/git.test.ts
@@ -2,17 +2,23 @@ import { describe, it } from 'node:test';
 import assert from 'node:assert';
 import { execSync } from 'node:child_process';
 import * as git from '../utils/git';
+import { join } from 'node:path';
 
 describe('git', () => {
-  it('should get the latest commit hash', () => {
-    const gitHash = execSync('git rev-parse HEAD').toString().trim();
-    const commitHash = git.getLastCommitHash();
-    assert.strictEqual(commitHash, gitHash);
-  });
+  const mocks = ["loose", "pack"];
+  mocks.forEach((mock) => {
+    const gitDir = join(__dirname, `./mocks/git/${mock}`);
+    it('should get the latest commit hash', () => {
+      const gitHash = "82f8d685e4619bd93dd86b6942ebdb344e3861c5";
+      const commitHash = git.getLastCommitHash(gitDir);
+      assert.strictEqual(commitHash, gitHash);
+    });
 
-  it('should get getLastCommitTimestamp', () => {
-    const gitTimestamp = execSync('git log -1 --format=%at').toString().trim();
-    const timestamp = git.getCommitTimestamp();
-    assert.strictEqual(timestamp, Number(gitTimestamp) * 1000);
+    it('should get getLastCommitTimestamp', () => {
+      const gitTimestamp = 1711062839;
+      const timestamp = git.getCommitTimestamp(null, gitDir);
+      // our timestamps have millisecond precision, because: JavaScript.
+      assert.strictEqual(timestamp, Number(gitTimestamp) * 1000);
+    });
   });
 });
